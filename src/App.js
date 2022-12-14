89237f07d05ea531d6f449c0dc95f57bf6de9027
Yasinta <yasinta.syara@gmail.com>
2022-12-14 23:02:13 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import './App.css';
-import {getMovieList, searchMovie} from './api'
+import { getMovieList, searchMovie } from './api'
 import { useEffect, useState } from 'react';
 
 const App = () => {
-const [popularMovies, setPopularMovies] = useState([])
+  const [popularMovies, setPopularMovies] = useState([])
 
   useEffect(() => {
-  getMovieList().then((result) => {
-    setPopularMovies(result)
-  })
+    getMovieList().then((result) => {
+      setPopularMovies(result)
+    })
   }, [])
 
   const PopularMovieList = () => {
     return popularMovies.map((movie, i) => {
       return (
         <div className='movie-wrapper' key={i}>
-            <div className='movie-title'>{movie.title}</div>
-            <img className='movie-image' src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
-            <div className='movie-date'>release: {movie.release_date}</div>
-            <div className='movie-rate'>{movie.vote_average}</div>
-          </div>
+          <div className='movie-title'>{movie.title}</div>
+          <img className='movie-image' src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
+          <div className='movie-date'>release: {movie.release_date}</div>
+          <div className='movie-rate'>{movie.vote_average}</div>
+        </div>
       )
     })
   }
 
-  const search = (q) => {
-  console.log({q})
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q)
+      setPopularMovies(query.results)
+    }
   }
 
 
@@ -35,9 +38,9 @@ const [popularMovies, setPopularMovies] = useState([])
         <h1>MOVIE MANIA</h1>
         <input placeholder='cari film favoritmu..'
           className='movie-search'
-          onChange={({target}) => search(target.value)} />
+          onChange={({ target }) => search(target.value)} />
         <div className='movie-container'>
-          <PopularMovieList/>
+          <PopularMovieList />
         </div>
       </header>
     </div>
